9ba7b38bf14f46c60b8099e022ad5037fe7790d0
wimukthi <9wimu9@gmail.com>
2023-01-19 23:14:31 +0530

    Create Collection model.

--- store/models.py
+++ store/models.py
@@ -46,3 +46,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
