dda5c98c0f742c9b0eba76c79f7ab0fec1c17ede
wimukthi <9wimu9@gmail.com>
2023-01-19 23:36:18 +0530

    Create Cart and CartItem models

--- store/models.py
+++ store/models.py
@@ -57,6 +57,16 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
