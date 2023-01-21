815e849dc21f6aa5b74c740abc3c350cc48053fb
wimukthi <9wimu9@gmail.com>
2023-01-21 20:42:19 +0530

    referencing fields using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,22 +1,39 @@
-from django.db.models import Q
+from django.db.models import Q, F
 from django.shortcuts import render
 from store.models import Product
 
 
 def say_hello(request):
-    # Products : inventory < 10 OR price < 20
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) | Q(unit_price__lt=20)
-    )
+    # referencing columns
+    # products that have equal inventory and price
 
-    # Products : inventory < 10 AND price < 20
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) & Q(unit_price__lt=20)
-    )
+    # this doesn't work
+    # queryset = Product.objects.filter(inventory=unit_price)
 
-    # Products : inventory < 10 AND price >= 20
-    queryset = Product.objects.filter(
-        Q(inventory__lt=10) & ~Q(unit_price__lt=20)
-    )
+    # using F objects
+    queryset = Product.objects.filter(inventory=F('unit_price'))
+    #     SELECT `store_product`.`id`,
+    #     `store_product`.`title`,
+    #     `store_product`.`slug`,
+    #     `store_product`.`description`,
+    #     `store_product`.`unit_price`,
+    #     `store_product`.`inventory`,
+    #     `store_product`.`last_update`,
+    #     `store_product`.`collection_id`
+    # FROM `store_product`
+    # WHERE `store_product`.`inventory` = (`store_product`.`unit_price`)
+
+    # F objects and referencing
+    queryset = Product.objects.filter(inventory=F('collection__id'))
+    #     SELECT `store_product`.`id`,
+    #     `store_product`.`title`,
+    #     `store_product`.`slug`,
+    #     `store_product`.`description`,
+    #     `store_product`.`unit_price`,
+    #     `store_product`.`inventory`,
+    #     `store_product`.`last_update`,
+    #     `store_product`.`collection_id`
+    # FROM `store_product`
+    # WHERE `store_product`.`inventory` = (`store_product`.`collection_id`)
 
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
