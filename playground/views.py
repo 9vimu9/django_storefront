359826a62e342857f27fee7c226927d53db73220
wimukthi <9wimu9@gmail.com>
2023-01-21 20:35:38 +0530

    MySql AND and OR operations with Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,22 @@
+from django.db.models import Q
 from django.shortcuts import render
 from store.models import Product
 
 
 def say_hello(request):
-    # task : get products inventory less than 10 and price less than 20
+    # Products : inventory < 10 OR price < 20
+    queryset = Product.objects.filter(
+        Q(inventory__lt=10) | Q(unit_price__lt=20)
+    )
 
-    # using multiple params
-    queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-    # using multiple filters
-    queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # Products : inventory < 10 AND price < 20
+    queryset = Product.objects.filter(
+        Q(inventory__lt=10) & Q(unit_price__lt=20)
+    )
+
+    # Products : inventory < 10 AND price >= 20
+    queryset = Product.objects.filter(
+        Q(inventory__lt=10) & ~Q(unit_price__lt=20)
+    )
 
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
