c5e193c78980dcf66063001ee7b0b3ca8093d9f4
wimukthi <9wimu9@gmail.com>
2023-01-21 23:04:52 +0530

    only and defer

--- playground/views.py
+++ playground/views.py
@@ -4,50 +4,16 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    # values returns dictionary
-    queryset = Product.objects.values('id', 'title')
-    # SELECT `store_product`.`id`,
-    # `store_product`.`title`
-    # FROM `store_product`
+    # values => dict
+    # values_list =>tuple
+    # only => model instances
+    # defer => model instances
 
-    # get values from related columns
-    # (one-to-many relationship is defined between Customer and Product models)
-    queryset = Product.objects.values('title', 'collection__title')
-    # SELECT `store_product`.`title`,
-    # `store_collection`.`title`
-    # FROM `store_product`
-    # INNER JOIN `store_collection`
-    # ON (`store_product`.`collection_id` = `store_collection`.`id`)
+    # ONLY
+    # read marked columns only
+    queryset = Product.objects.only('id', 'title')
 
-    # values == values_list
-    # values returns dict
-    # values_list returns tuple
-    queryset = Product.objects.values_list('title')
-
-    # QUESTION
-    # ---------
-    # select products that have been ordered and sort them by title
-    queryset = OrderItem.objects.order_by('product__title').distinct().values('product__title')
-    # but this returns array of dictionaries after the list. and we need array of products
-    # for the html views. not OrderItem s
-    queryset = Product.objects.filter(
-        id__in=OrderItem.objects.distinct().values('product__id')
-    ).order_by('title')
-    # id__in === WHERE id IN
-
-    # SELECT `store_product`.`id`,
-    # `store_product`.`title`,
-    # `store_product`.`slug`,
-    # `store_product`.`description`,
-    # `store_product`.`unit_price`,
-    # `store_product`.`inventory`,
-    # `store_product`.`last_update`,
-    # `store_product`.`collection_id`
-    # FROM `store_product`
-    # WHERE `store_product`.`id` IN (
-    #     SELECT DISTINCT U0.`product_id`
-    # FROM `store_orderitem` U0
-    # )
-    # ORDER BY `store_product`.`title` ASC
-
+    # DEFER
+    # marked columns won't be added
+    queryset = Product.objects.defer('description')
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
