7a92f38d1e2439201908264cb6c35fcc1f608a7e
wimukthi <9wimu9@gmail.com>
2023-01-22 17:24:41 +0530

    select_related

--- playground/views.py
+++ playground/views.py
@@ -4,5 +4,11 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    queryset = Product.objects.all()
+    # select_related can be used to connect 2 tables
+    # with one-to-many relationship
+    queryset = Product.objects.select_related('collection').all()
+
+    # let's say collection has a relationship with a table, and
+    # we need access to it.
+    queryset = Product.objects.select_related('collection__someothertable').all()
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
