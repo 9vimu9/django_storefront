b61d410be6c9f88234df80ca051aa4ce26029b90
wimukthi <9wimu9@gmail.com>
2023-01-22 17:19:06 +0530

    accessing data of other tables without letting know Django about it.

--- playground/views.py
+++ playground/views.py
@@ -4,16 +4,5 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    # values => dict
-    # values_list =>tuple
-    # only => model instances
-    # defer => model instances
-
-    # ONLY
-    # read marked columns only
-    queryset = Product.objects.only('id', 'title')
-
-    # DEFER
-    # marked columns won't be added
-    queryset = Product.objects.defer('description')
+    queryset = Product.objects.all()
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
