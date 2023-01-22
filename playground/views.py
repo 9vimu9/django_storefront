d4b99a13e70f1f966006728d7d0d4588c7bca9d1
wimukthi <9wimu9@gmail.com>
2023-01-22 23:28:04 +0530

    Django database functions

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,24 @@
-from django.db.models import Q, F, Count, Min, Value
+from django.db.models import Q, F, Count, Min, Value, Func
+from django.db.models.functions import Concat
 from django.shortcuts import render
-from store.models import Product, OrderItem, Order
+from store.models import Product, OrderItem, Order, Customer
 
 
 def say_hello(request):
-    # add columns temporarily
-    queryset = Product.objects.annotate(temp_column=Value('12'))
-    # SELECT `store_product`.`id`,
+    # CONCAT using Func object
+    queryset = Customer.objects.annotate(
+        full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+    )
+    # SELECT `store_customer`.`id`,
     # ...
-    # '12' AS `temp_column`
-    # FROM `store_product`
+    # CONCAT(`store_customer`.`first_name`, ' ', `store_customer`.`last_name`) AS `full_name`
+    # FROM `store_customer`
 
-    queryset = Product.objects.annotate(new_title=F('title'))
-    # SELECT `store_product`.`id`,
-    # ...
-    # `store_product`.`title` AS `new_title`
-    # FROM `store_product`
+    # CONCAT using Concat object
+    queryset = Customer.objects.annotate(
+        full_name=Concat('first_name', Value(' '), 'last_name')
+    )
+    # more info about Django database functions
+    # https://docs.djangoproject.com/en/4.1/ref/models/database-functions/
 
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
