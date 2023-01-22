be699fd45a07f651cf2265ee636d7151e2fef5c1
wimukthi <9wimu9@gmail.com>
2023-01-22 22:08:14 +0530

    aggregate

--- playground/views.py
+++ playground/views.py
@@ -1,37 +1,21 @@
-from django.db.models import Q, F
+from django.db.models import Q, F, Count, Min
 from django.shortcuts import render
 from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
-    # TASK
-    # -----
-    # get the last 5 orders with their customer and items, including products
+    result = Product.objects. \
+        aggregate(total_products=Count('id'))
+    # {'total_products': 1000}
 
-    # step - 1
-    # we want list of orders, therefore we should start with Order model.
-    # first get last 5 orders
-    queryset = Order.objects.order_by('-placed_at')[:5]
+    result = Product.objects. \
+        aggregate(total_products=Count('id'), minimum_price=Min('unit_price'))
+    # {'total_products': 1000, 'minimum_price': Decimal('1.06')}
 
-    # step - 2 get customers of those orders
-    queryset = Order.objects.select_related('customer'). \
-                   order_by('-placed_at')[:5]
-    # single order can only have 1 customer only. therefore select_related is used.
-
-    # step -3 preload items with the orders
-    queryset = Order.objects.select_related('customer'). \
-                   prefetch_related('orderitem_set'). \
-                   order_by('-placed_at')[:5]
-    # store_orderitem table has order_id column.
-    # therefore we can get order detail from an order item.
-    # vice versa, we should be able to get order items from an order as well.
-    # In order to do that we can use 'orderitem_set'
-
-    # single order can have multiple order items therefore prefetch_related is used
-
-    # step - 4 load products referencing in OrderItem
-    queryset = Order.objects.select_related('customer'). \
-                   prefetch_related('orderitem_set__product'). \
-                   order_by('-placed_at')[:5]
+    # aggregate can be done on queryset
+    result = Product.objects. \
+        filter(collection__id=5). \
+        aggregate(total_products=Count('id'), minimum_price=Min('unit_price'))
+    # {'total_products': 245, 'minimum_price': Decimal('1.08')}
 
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
