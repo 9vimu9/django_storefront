1968f865f9fe9e5e6105d635c0b36e6bc7c1cd59
wimukthi <9wimu9@gmail.com>
2023-01-28 23:49:33 +0530

    update query

--- playground/views.py
+++ playground/views.py
@@ -9,23 +9,22 @@ from tags.models import TaggedItem
 def say_hello(request):
     # method 1
     # ===========
-    collection = Collection()
-    collection.title = 'games'
-    # assigning Product object to 'feature_product' field
-    collection.featured_product = Product(pk=1)
-    # or
-    # directly assign value to the column
-    collection.featured_product_id = 1
+    # get object from database that is wants to update
+    collection = Collection.objects.get(pk=11)
+    collection.title = 'games updated'
+    # remove featured product
+    collection.featured_product = None
     collection.save()
 
     # method 2
     # ===========
-    collection = Collection.objects.create(
+    Collection.objects.filter(pk=11).update(
         title='games',
-        featured_product=Product(pk=1)
     )
-    # INSERT INTO `store_collection` (`title`, `featured_product_id`)
-    # VALUES ('games', 1)
-    queryset = Product.objects.all()
+    # using this method, we can update multiple rows at once.
+
+    # UPDATE `store_collection`
+    # SET `title` = 'games'
+    # WHERE `store_collection`.`id` = 11
 
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
