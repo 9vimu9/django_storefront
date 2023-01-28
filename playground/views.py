7f0cde50f406d6209501bd1eb0b80e71e20e150c
wimukthi <9wimu9@gmail.com>
2023-01-28 17:42:30 +0530

    Expression wrapper.

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,18 @@
-from django.db.models import Q, F, Count, Min, Value, Func
+from django.db.models import Q, F, Count, Min, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.shortcuts import render
 from store.models import Product, OrderItem, Order, Customer
 
 
 def say_hello(request):
-    queryset = Product.objects.annotate(
-        discounted_price=F('unit_price')*0.8
+    discounted_price = ExpressionWrapper(
+        F('unit_price') * 0.8, output_field=DecimalField()
     )
-    # this will return following error
-    # Cannot infer type of '*' expression involving these
-    # types: DecimalField, FloatField. You must set output_field.
+    # SELECT `store_product`.`id`,
+    # ...
+    # (`store_product`.`unit_price` * 0.8e0) AS `discounted_price`
+    # FROM `store_product`
+    queryset = Product.objects.annotate(
+        discounted_price=discounted_price
+    )
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
