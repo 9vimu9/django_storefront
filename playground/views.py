9246a2b68e679be609b4b9800fd3c7523068f618
wimukthi <9wimu9@gmail.com>
2023-01-28 16:58:05 +0530

    Expression wrapper : why need a one.

--- playground/views.py
+++ playground/views.py
@@ -5,26 +5,10 @@ from store.models import Product, OrderItem, Order, Customer
 
 
 def say_hello(request):
-    # task :
-    # add temporary column orders_count
-    # store order count for that customer in orders_count column
-
-    # Order model has customer Foreign Key
-    # we can take customer of an order (order --> customer)
-    # inversely we should be able to  get orders of a customer as well (customer --> Orders)
-    # we can use `order_set` to do it
-
-    # but here it doesn't work
-    # order_set --> order
-
-    # SELECT `store_customer`.`id`,
-    # ...
-    # COUNT(`store_order`.`id`) AS `orders_count`
-    # FROM `store_customer`
-    # LEFT OUTER JOIN `store_order`
-    # ON (`store_customer`.`id` = `store_order`.`customer_id`)
-    # GROUP BY `store_customer`.`id`
-    # ORDER BY NULL
-
-    queryset = Customer.objects.annotate(orders_count=Count('order'))
+    queryset = Product.objects.annotate(
+        discounted_price=F('unit_price')*0.8
+    )
+    # this will return following error
+    # Cannot infer type of '*' expression involving these
+    # types: DecimalField, FloatField. You must set output_field.
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
