360b759314c6e0cb1547341ca70b0188cbde1f89
wimukthi <9wimu9@gmail.com>
2023-01-28 23:22:34 +0530

    Querying generic relationships : refactoring code using model managers.

--- playground/views.py
+++ playground/views.py
@@ -7,34 +7,5 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    # Task :
-    # get tagged tags for a product.
-
-    # Answer :
-    # since Tag model is located in another application,
-    # we have to use generic relationships to get data.
-    #
-
-    #  get content type id for a model
-    content_type = ContentType.objects.get_for_model(Product)
-    # SELECT `django_content_type`.`id`,
-    # `django_content_type`.`app_label`,
-    # `django_content_type`.`model`
-    # FROM `django_content_type`
-    # WHERE (`django_content_type`.`app_label` = 'store' AND `django_content_type`.`model` = 'product') LIMIT 21
-
-    random_product_id = 1
-    queryset = TaggedItem.objects. \
-        select_related('tag') \
-        .filter(
-            content_type=content_type,  # set content type id
-            object_id=random_product_id
-        )
-    # SELECT `tags_taggeditem`.`id`,
-    # ...
-    # FROM `tags_taggeditem`
-    # INNER JOIN `tags_tag`
-    # ON (`tags_taggeditem`.`tag_id` = `tags_tag`.`id`)
-    # WHERE (`tags_taggeditem`.`content_type_id` = 12 AND `tags_taggeditem`.`object_id` = 1)
-
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
--- tags/models.py
+++ tags/models.py
@@ -3,11 +3,27 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
 
+# customized model manager
+class TaggedItemManager(models.Manager):
+
+    # created a method inside the new Model manager to get Tagged
+    # items for a certain model
+    def get_tags_for(self, object_type, object_id):
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+                content_type=ContentType.objects.get_for_model(object_type),
+                object_id=object_id
+            )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
+    # assigned customized model manager to objects attribute.
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
