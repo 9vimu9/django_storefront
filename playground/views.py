b997ef510393c309a5b58806a1124187a1d76175
wimukthi <9wimu9@gmail.com>
2023-01-28 23:36:02 +0530

    insert query

--- playground/views.py
+++ playground/views.py
@@ -2,10 +2,30 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q, F, Count, Min, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.shortcuts import render
-from store.models import Product, OrderItem, Order, Customer
+from store.models import Product, OrderItem, Order, Customer, Collection
 from tags.models import TaggedItem
 
 
 def say_hello(request):
-    queryset = TaggedItem.objects.get_tags_for(Product, 1)
+    # method 1
+    # ===========
+    collection = Collection()
+    collection.title = 'games'
+    # assigning Product object to 'feature_product' field
+    collection.featured_product = Product(pk=1)
+    # or
+    # directly assign value to the column
+    collection.featured_product_id = 1
+    collection.save()
+
+    # method 2
+    # ===========
+    collection = Collection.objects.create(
+        title='games',
+        featured_product=Product(pk=1)
+    )
+    # INSERT INTO `store_collection` (`title`, `featured_product_id`)
+    # VALUES ('games', 1)
+    queryset = Product.objects.all()
+
     return render(request, 'hello.html', {'name': 'Vim', 'products': list(queryset)})
