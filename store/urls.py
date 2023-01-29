b21b75bb711081b2a88e9913bbebdc928e80c06a
wimukthi <9wimu9@gmail.com>
2023-01-29 15:24:39 +0530

    create collection_detail controller function and route

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail)  # throws 404 if id is not an int
+    path('products/<int:id>/', views.product_detail),  # throws 404 if id is not an int
+    path('collections/<int:pk>/', views.collection_detail, name='collection-detail')  # throws 404 if id is not an int
 ]
