83b05a13c22bec1c768a9473e6c65b1705e453c8
wimukthi <9wimu9@gmail.com>
2023-01-29 14:14:43 +0530

    introduce new field into product serializer that is does not exist in Product model

--- store/serializers.py
+++ store/serializers.py
@@ -1,15 +1,16 @@
+from _decimal import Decimal
 from rest_framework import serializers
 
+from store.models import Product
 
-# https://www.django-rest-framework.org/api-guide/serializers/
-
-# serializer
-# converts a model instance to a dictionary.
-# model -> data internal representation
-# serializer -> data outside representation
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=4)
-    # serializer name has to be same as model attribute
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        # this will throw an error, decimal cant multiply with float
+        # return product.unit_price*1.1
+        return product.unit_price * Decimal(1.1)
