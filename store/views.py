45ff3a480c8a8f73b637ea831afd3db48b898a77
wimukthi <9wimu9@gmail.com>
2023-01-29 13:38:34 +0530

    return 404 when there is no model

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -13,6 +14,10 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    try:
+        product = Product.objects.get(pk=id)
+        # get throws DoesNotExist exception if no model is to be found.
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
