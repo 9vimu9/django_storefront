cedc7ee9c7e75231e4210275d133aecbc00db394
wimukthi <9wimu9@gmail.com>
2023-01-29 14:36:17 +0530

    Serializing relationships : Get collction title instead of collection id - wrong way
    we lazy loaded collections in the view

--- store/serializers.py
+++ store/serializers.py
@@ -9,10 +9,11 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=4, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # Collection is a primary key field in Product (collection_id)
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset=Collection.objects.all()
-    )
+    # this will get Collection model's string representation.
+    # string representation of the Collection is its title[see Collection class in models.php],
+    # therefore we get title of the associated collection of the product
+    collection = serializers.StringRelatedField()
+
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
