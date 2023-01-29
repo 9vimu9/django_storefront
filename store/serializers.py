ed88b2e641e98d994989122185ec8fd5766061ea
wimukthi <9wimu9@gmail.com>
2023-01-29 14:43:26 +0530

    Serializing relationships : adding nested Collection object in each Product JSON object

--- store/serializers.py
+++ store/serializers.py
@@ -4,15 +4,15 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=4, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # this will get Collection model's string representation.
-    # string representation of the Collection is its title[see Collection class in models.php],
-    # therefore we get title of the associated collection of the product
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
 
     def calculate_tax(self, product: Product):
