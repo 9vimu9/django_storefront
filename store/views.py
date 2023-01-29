bdd6511e089dc1bf99d6d6f03cb61eeb47146d46
wimukthi <9wimu9@gmail.com>
2023-01-29 17:26:08 +0530

    Deserializing objects : point GET and POST requests to same function product_list

--- store/views.py
+++ store/views.py
@@ -6,13 +6,22 @@ from store.models import Product
 from store.serializers import ProductSerializer
 
 
-@api_view()
+@api_view(['GET', 'POST'])
+# all the POST and GET requests come to
+# /store/products will be redirected to the product_list
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True, context={
-        'request': request
-    })
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={
+            'request': request
+        })
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        # if the request is POST we can use the same serializer to convert
+        # request using 'data' keyword
+        serializer = ProductSerializer(data=request.data)
+        return Response('ok')
 
 
 @api_view()
