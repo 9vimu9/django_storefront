18b141e1bda289a72355e566734150bb25c8a112
wimukthi <9wimu9@gmail.com>
2023-02-04 20:10:36 +0530

    Review : created nested routes https://github.com/alanjds/drf-nested-routers pipenv install drf-nested-routers

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,29 @@
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
 from . import views
 
-router = SimpleRouter()
-router.register('products', views.ProductViewSet)  # SimpleRouter is used to create routes with ViewSet
+router = routers.SimpleRouter()  # SimpleRouter replaced by NestedSimpleRouter
+router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
+# setup nested router
+products_router = routers.NestedSimpleRouter(router, 'products', lookup='product_pk')
+"""
+router -> parent router
+products -> pre-fix of the parent router that is used during parent router registration.
+lookup='product_pk' -> The regex variable that matches an instance of the parent-resource
+will be called '<lookup>_<parent-viewset.lookup_field>'
+In the example above, lookup=domain and the parent viewset looks up
+on 'pk' so the parent lookup regex will be 'domain_pk'.
+Default: 'nested_<n>' where <n> is 1+parent_router.nest_count
+"""
+# register child route in the nested parent route
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+"""
+basename-> prefix that is used to generate URL patterns
+so route names will be called product-reviews-detail, product-reviews-list .etc
+"""
+# http://localhost:8000/store/products/122/reviews/1/
+
 # URLConf
-urlpatterns = router.urls  # urls attribute returns array of patterns of ViewSets.
-# therefore we can directly assign it to urlpatterns
+urlpatterns = router.urls + products_router.urls
