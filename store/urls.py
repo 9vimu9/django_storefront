561a7da9b1db3aea9e09eab8ffb5e192be9e9f51
wimukthi <9wimu9@gmail.com>
2023-02-04 16:16:45 +0530

    Used SimpleRouter to add ViewSet routes

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
+
 from . import views
 
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)  # SimpleRouter is used to create routes with ViewSet
+router.register('collections', views.CollectionViewSet)
+
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-    path('collections/', views.CollectionList.as_view()),
-]
+urlpatterns = router.urls  # urls attribute returns array of patterns of ViewSets.
+# therefore we can directly assign it to urlpatterns
