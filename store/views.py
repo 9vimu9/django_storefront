de57eb776aef32897c744c9d10503f600dfd91a3
wimukthi <9wimu9@gmail.com>
2023-02-05 00:43:58 +0530

    Sorting

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -14,11 +14,10 @@ from store.serializers import ProductSerializer, CollectionSerializer, ReviewSer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]  # Added Search Filter
-    filterset_class = ProductFilter  # set filter class
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
     search_fields = ['title', 'description']
-    # adding fields to be searched. input string will be searched in those columns
-    # http://localhost:8000/store/products/?search=QUERY
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
